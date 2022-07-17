46c99f1905621f0ef8e1b3bf3b3c171864ca2716
wfy <kaliawng@163.com>
2022-07-17 18:17:13 +0800

    note with oss

---
+++ src/main/java/com/example/notewithoss/NoteWithOssApplication.java
@@ -0,0 +1,13 @@
+package com.example.notewithoss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteWithOssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteWithOssApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/notewithoss/NoteWithOssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.notewithoss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteWithOssApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
