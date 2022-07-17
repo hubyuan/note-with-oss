87c149b65c0b545b6e2c4f36bf7a2eb7e43b0937
wfy <kaliawng@163.com>
2022-07-17 18:30:46 +0800

    note with oss

--- src/main/java/com/example/notewithoss/NoteWithOssApplication.java
+++ src/main/java/com/example/notewithoss/NoteWithOssApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class NoteWithOssApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NoteWithOssApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(NoteWithOssApplication.class, args);
+    }
 
 }
--- src/test/java/com/example/notewithoss/NoteWithOssApplicationTests.java
+++ src/test/java/com/example/notewithoss/NoteWithOssApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class NoteWithOssApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
